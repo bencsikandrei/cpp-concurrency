4a212033cd3f43fd5cb53da6bc9644f175375b2f
Andrei BENCSIK <andrei.bencsik@gmail.com>
2018-10-07 15:17:08 +0300

    [Algos] Parallel accumulate with futures and async instead of raw threads and a benchmark function

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -1,5 +1,7 @@
 #pragma once 
 #include <thread>
+#include <chrono>
+#include <utility>
 
 struct NonCopyable {
     NonCopyable() = default;
@@ -11,4 +13,16 @@ struct NonCopyable {
 template <typename Func, typename ... Args>
 std::thread make_thread(Func&& f, Args&& ... args) {
     return std::thread(std::forward<Func>(f), std::forward<Args>(args)...);
+}
+
+using TimePoint = std::chrono::high_resolution_clock::time_point;
+
+
+template<typename F, typename... Args>
+double bench(F func, Args&&... args)
+{
+    TimePoint start = std::chrono::high_resolution_clock::now();
+    func(std::forward<Args>(args)...);
+    TimePoint end =  std::chrono::high_resolution_clock::now();
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
 }
